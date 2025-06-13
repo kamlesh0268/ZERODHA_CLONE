70bd087600532f91ae27c3f93d4d976f7958fa2d
kamlesh0268 <kamlesh.choudhary9042@gmail.com>
2025-06-13 13:35:16 +0530

    Updated: package-lock.json and fixed issues

--- frontend/src/Landing_page/Footer.js
+++ frontend/src/Landing_page/Footer.js
@@ -5,7 +5,11 @@ function Footer() {
         <div className="container border-top mt-5 " >
             <div className="row mt-5">
                 <div className="col">
-                    <img src="media/IMAGES/images/logo.svg"  style={{ width: "50%" }} />
+                    <img 
+                        src="media/IMAGES/images/logo.svg" 
+                        alt="Zerodha logo in blue text with a stylized Z symbol, representing the Zerodha financial services brand, displayed on a clean white background" 
+                        style={{ width: "50%" }} 
+                    />
                     <p>&copy; 2010 - 2025, Zerodha Broking Ltd.
 
 All rights reserved.</p>
@@ -56,7 +60,7 @@ All rights reserved.</p>
 
 <p>Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.</p>
 
-<p>"Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.</p>
+<p>Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary. Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.</p>
 </div>
         </div>
         </footer>
